120808343b5ec29611432d00eb15772ae0c7d025
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-01-13 23:55:31 +0100

    Turned api-clients/cs-fixer-config into wyrihaximus/cs-fixer-config

---
+++ src/rules_include.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\CsFixerConfig;
+
+if (!\defined('WyriHaximus\CsFixerConfig\RULES')) {
+    require __DIR__ . \DIRECTORY_SEPARATOR . 'rules.php';
+}
